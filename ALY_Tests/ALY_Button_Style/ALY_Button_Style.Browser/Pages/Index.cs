8a32faa0aee679ed2c28b51399caddddc86c593b
kernel-coder <kernel.writer@gmail.com>
2022-03-15 09:25:35 +0600

    Initial Commit

---
+++ ALY_Tests/ALY_Button_Style/ALY_Button_Style.Browser/Pages/Index.cs
@@ -0,0 +1,26 @@
+﻿using ALY_Button_Style.Browser.Interop;
+using DotNetForHtml5;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.JSInterop;
+
+namespace ALY_Button_Style.Browser.Pages
+{
+    [Route("/")]
+    public class Index : ComponentBase
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder __builder)
+        {
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Cshtml5Initializer.Initialize(new UnmarshalledJavaScriptExecutionHandler(JSRuntime));
+            Program.RunApplication();
+        }
+
+        [Inject]
+        private IJSRuntime JSRuntime { get; set; }
+    }
+}
---
+++ ALY_Tests/ALY_Button_Style/ALY_Button_Style.Simulator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using OpenSilver.Simulator;
+using System;
+
+namespace ALY_Button_Style.Simulator
+{
+    internal static class Startup
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            return SimulatorLauncher.Start(typeof(App));
+        }
+    }
+}

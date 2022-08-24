d6d67d0660722aa0b262db4c784c0a82a49b4380
kernel-coder <kernel.writer@gmail.com>
2022-08-24 01:21:33 -0400

    Views updated

---
+++ ALY_Tests/ALY_Button_Style/ALY_Button_Style/Views/MediSpanMedication.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace Virtuoso.Core.Controls
+{
+    public partial class MediSpanMedication : UserControl
+    {
+        public MediSpanMedication()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
